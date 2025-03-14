0dbdfad204df69da7326b85dd4d15acb19ee03d3
Jongkyun <hjk326333@gmail.com>
2025-03-14 09:18:21 +0900

    Day01. 문자열 다루기 진행 중

---
+++ src/string/Solution2.java
@@ -0,0 +1,22 @@
+package string;
+
+import java.util.Scanner;
+
+public class Solution2 {
+
+    public static void main(String[] args) {
+        Scanner scan = new Scanner(System.in);
+        int num = scan.nextInt();
+        String[] revers_input = new String[num];
+
+        for (int i = 0; i < num; i++) {
+            String input = scan.next();
+            revers_input[i] = new StringBuilder(input).reverse().toString();
+        }
+
+        for (int i = 0; i < num; i++) {
+            System.out.println(revers_input[i]);
+        }
+        scan.close();
+    }
+}
