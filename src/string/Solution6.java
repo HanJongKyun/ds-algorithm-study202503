f9e330e9bcee82ae4aba6b9bdcd66aabdd8c2fa5
Jongkyun <hjk326333@gmail.com>
2025-03-14 15:42:30 +0900

    Day02 - 문자열 다루기

--- src/string/Solution6.java
+++ src/string/Solution6.java
@@ -1,22 +1,41 @@
 package string;
 
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
 import java.util.Scanner;
+import java.util.StringTokenizer;
 
 public class Solution6 {
 
-    public static void main(String[] args) {
+    public static void main(String[] args) throws IOException {
+        /*
         Scanner scan = new Scanner(System.in);
         String str = scan.nextLine();
         String[] arr = str.split(" ");
         String s = arr[0];
         char t = arr[1].charAt(0);
+        */
+        
+        // teachermode e
+        // Scanner는 정규표현식을 처리하는 등 기능이 많아서, 추가적인 연산이 필요해서
+        // 입력이 자주 발생하면 속도가 저하됩니다.
+        // BufferedReader는 단순히 입력값 자체를 읽어오는 거라 연산이 필요 없어 속도가 빠릅니다.
+        // readLine() 메서드를 통해 한 줄씩 텍스트를 읽어 옵니다.
+        // 만약 한 줄에 종류가 다른 입력값이 여럿 존재한다면, StringTokenizer를 통해서
+        // 구분자를 통해 각 입력값을 토큰화 해서 가져올 수 있습니다.
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine(), " ");
+        String s = st.nextToken();
+        char c = st.nextToken().charAt(0);
+
 
         int[] result = new int[s.length()]; // 거리를 숫자로 담을 배열
         int p = 100; // 거리 비교를 위해 선언한 변수 (0으로 초기화하면 안됨! 0은 찾고자 하는 문자와 같다는 의미)
 
         // 왼쪽에서 오른쪽으로 거리 계산
         for (int i = 0; i < s.length(); i++) {
-            if (s.charAt(i) == t) {
+            if (s.charAt(i) == c) {
                 p = 0;
             } else {
                 p++; // 기존 문자가 아니라면 이전 문자와의 거리 + 1
@@ -27,7 +46,7 @@ public class Solution6 {
         // 오른쪽에서 왼쪽으로 거리 계산
         p = 100;
         for (int i = s.length() - 1; i >= 0; i--) {
-            if (s.charAt(i) == t) {
+            if (s.charAt(i) == c) {
                 p = 0;
             } else {
                 p++; // 기존 문자가 아니라면 이전 문자와의 거리 + 1
@@ -39,6 +58,6 @@ public class Solution6 {
         for (int i : result) {
             System.out.print(i + " ");
         }
-        scan.close();
+        br.close();
     }
 }
--- src/string/Solution7.java
+++ src/string/Solution7.java
@@ -1,11 +1,17 @@
 package string;
 
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
 import java.util.Scanner;
 
 public class Solution7 {
-    public static void main(String[] args) {
-        Scanner scan = new Scanner(System.in);
-        String str = scan.next();
+    public static void main(String[] args) throws IOException {
+//        Scanner scan = new Scanner(System.in);
+//        String str = scan.next();
+
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        String str = br.readLine();
         StringBuilder result = new StringBuilder();
 
         int cnt = 1;
@@ -21,12 +27,12 @@ public class Solution7 {
             }
         }
 
-        // 마지막 문자 처리 반복 횟수를 추가
+        // 마지막 문자 처리
         result.append(str.charAt(str.length() - 1));
         if (cnt > 1) {
             result.append(cnt);
         }
-
         System.out.println(result);
+        br.close();
     }
 }
