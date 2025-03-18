7c17615839f749d3cf7bcef1d5164f51f17a3f3f
Jongkyun <hjk326333@gmail.com>
2025-03-18 09:19:40 +0900

    Day03. 재귀 기초

---
+++ src/recursive/RecursiveBasic.java
@@ -0,0 +1,17 @@
+package recursive;
+
+public class RecursiveBasic {
+
+    public static void recursive(int n) {
+        if (n == 0) {
+            return;
+        }
+        recursive(n - 1);
+        System.out.println(n + " ");
+    }
+
+
+    public static void main(String[] args) {
+        recursive(3);
+    }
+}
